bdcba6aae3756683f69ac34271c1bdc9e5ac4822
AChristian N <31229894+Tochiskool@users.noreply.github.com>
2025-06-08 08:39:54 +0200

    Zoom in added to skill

--- src/pages/Skills/Skills.jsx
+++ src/pages/Skills/Skills.jsx
@@ -1,26 +1,26 @@
-import React, { useState } from "react";
-import "./skills.css";
+import React, { useState } from 'react';
+import './skills.css';
 const Skills = () => {
   const [icons, setIcons] = useState([
-    "/assets/skills/1react.png",
-    "/assets/skills/2mongo.png",
-    "/assets/skills/4mysql.png",
-    "/assets/skills/5express.png",
-    "/assets/skills/6node.png",
-    "/assets/skills/git.png",
-    "/assets/skills/07redux.jpeg",
-    "/assets/skills/08-tailwindcss-logo.png",
-    "/assets/skills/figma.png",
-    "/assets/skills/workbench.webp",
+    '/assets/skills/1react.png',
+    '/assets/skills/2mongo.png',
+    '/assets/skills/4mysql.png',
+    '/assets/skills/5express.png',
+    '/assets/skills/6node.png',
+    '/assets/skills/git.png',
+    '/assets/skills/07redux.jpeg',
+    '/assets/skills/08-tailwindcss-logo.png',
+    '/assets/skills/figma.png',
+    '/assets/skills/workbench.webp',
   ]);
 
   const displayIcons = icons.map((icon, index) => (
-    <li key={index}>
+    <li data-aos="zoom-in" key={index}>
       <img src={icon} alt={`Skill ${index + 1}`} />
     </li>
   ));
   return (
-    <div className='skillContainer'>
+    <div className="skillContainer">
       <h1>Skills Overview</h1>
       <h4>
         Here are a list of tools, libraries and platforms I have had previous
