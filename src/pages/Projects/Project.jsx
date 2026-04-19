5f5c7bb35cd56748732aeb25327d8ffda180efad
AChristian N <31229894+Tochiskool@users.noreply.github.com>
2026-04-19 02:03:11 +0200

    New project added

--- src/pages/Projects/Project.jsx
+++ src/pages/Projects/Project.jsx
@@ -1,6 +1,6 @@
-import React from "react";
-import ProjectDetails from "./ProjectDetails/ProjectDetails";
-import "./project.css";
+import React from 'react';
+import ProjectDetails from './ProjectDetails/ProjectDetails';
+import './project.css';
 const Project = () => {
   const projectData = [
     // {
@@ -13,11 +13,19 @@ const Project = () => {
     // },
     {
       id: 1,
-      name: "Zanolla Impianti Dashboard",
-      image: "/projectImages/zanola.png",
-      stack: "react vite|css3|ajax",
-      skill: "An amazing dashboard with a display of css technique.",
-      link: "https://zanola-impianti-dashboard.vercel.app/",
+      name: 'CAJLINKERS LOGISTICS AND MARKETPLACE ',
+      image: '/projectImages/cajLinkers.png',
+      stack: 'MERN STACK APP',
+      skill: 'REACT Node.js APIS JWT/AUTH/ REST API MONGO',
+      link: 'https://cajlinkersb2b.vercel.app/',
+    },
+    {
+      id: 2,
+      name: 'Zanolla Impianti Dashboard',
+      image: '/projectImages/zanola.png',
+      stack: 'react vite|css3|ajax',
+      skill: 'An amazing dashboard with a display of css technique.',
+      link: 'https://zanola-impianti-dashboard.vercel.app/',
     },
 
     // {
@@ -29,32 +37,33 @@ const Project = () => {
     //   link: "https://pdas-psi.vercel.app/",
     // },
     {
-      id: 2,
-      name: "Ace Health Care Solution",
-      image: "/projectImages/nelo.png",
-      stack: "react|express|css3|node|mongoDB",
-      skill: "Fluid website built with mobile first principles  ",
-      link: "https://acehealthcaresolution.org/",
+      id: 3,
+      name: 'Ace Health Care Solution',
+      image: '/projectImages/nelo.png',
+      stack: 'react|express|css3|node|mongoDB',
+      skill: 'Fluid website built with mobile first principles  ',
+      link: 'https://acehealthcaresolution.org/',
     },
     {
-      id: 3,
-      name: "An Online Ecommerce shop",
-      image: "/projectImages/ecom.png",
-      stack: "react|express|css3|node|mongoDB",
-      skill: "Mobile first ecommerce web application. Work in Progress ",
-      link: "https://tlinker-frontend.vercel.app/",
+      id: 4,
+      name: 'An Online Ecommerce shop',
+      image: '/projectImages/ecom.png',
+      stack: 'react|express|css3|node|mongoDB',
+      skill: 'Mobile first ecommerce web application. Work in Progress ',
+      link: 'https://tlinker-frontend.vercel.app/',
     },
-
   ];
   console.log(projectData);
   return (
-    <div className='mainProjectContainer'>
+    <div className="mainProjectContainer">
       <h1>
         Check out my work-in-progress projects built with a mobile-first
         approach, responsive layouts, and scalable architecture.
       </h1>
-      <h2 style={{fontStyle:"italic"}}>Project count: {projectData.length}</h2>
-      <div className='projectsContainer'>
+      <h2 style={{ fontStyle: 'italic' }}>
+        Project count: {projectData.length}
+      </h2>
+      <div className="projectsContainer">
         <ProjectDetails projects={projectData} />
       </div>
     </div>
